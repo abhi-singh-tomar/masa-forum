7b7406f49b639f174b86e2a9d403295214282ec6
abhi-singh-tomar <tomarabhishek8103@gmail.com>
2025-04-06 14:53:57 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true, unique: true }, // ✅ Ensure correct field name
   password: { type: String, required: true },
-  role: { type: String, enum: ["mentor", "student", "investor", "startup"], required: true }
 });
 
 const User = mongoose.model("User", userSchema);
